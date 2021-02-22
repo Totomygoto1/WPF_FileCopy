77dfaf0c05c0f1520418006ebf290591b36488f8
Totomygoto1 <katta36@hotmail.com>
2021-02-22 14:16:36 +0100

    adding files

---
+++ CopyDirectory/FileCopyFacility.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopyDirectory
+{
+    public class FileCopyFacility 
+    {
+        public List<string> _listFiles = new List<string>();
+        public string message = "";
+
+
+        public int DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
+        {
+
+            try
+            {
+                DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+                DirectoryInfo[] dirs = dir.GetDirectories();
+
+                FileInfo[] files = dir.GetFiles();
+
+                if (!Directory.Exists(destDirName))
+                {
+                    try
+                    {
+                        Directory.CreateDirectory(destDirName);
+
+                    }
+                    catch (Exception e)
+                    {
+                        message = "Please make sure drive and folder path is correct for destination folder";
+                        return 1;
+                    }
+                }
+
+                foreach (FileInfo file in files)
+                {
+                    string temppathfrom = Path.Combine(sourceDirName, file.Name);
+                    string temppath = Path.Combine(destDirName, file.Name);
+                    file.CopyTo(temppath, false);
+                    _listFiles.Add("Copy from: " + temppathfrom + " Copy to: " + temppath);
+                }
+
+                if (copySubDirs)
+                {
+
+                    foreach (DirectoryInfo subdir in dirs)
+                    {
+                        string temppathfrom = Path.Combine(sourceDirName, subdir.Name);
+                        string temppath = Path.Combine(destDirName, subdir.Name);
+                        DirectoryCopy(subdir.FullName, temppath, copySubDirs);
+                        _listFiles.Add("Copy from: " + temppathfrom + " Copy to: " + temppath);
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+                message = "Please make sure drive and folder path is correct for source folder";
+                return 1;
+            }
+
+            return 0;
+
+        }
+
+
+    }
+}
---
+++ WPF_FileCopy/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CopyDirectory;
+
+namespace WPF_FileCopy
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCopyFiles_Click(object sender, RoutedEventArgs e)
+        {
+            // txtDestination.Text = " C:\\CopiedFiles ";
+
+            lblMessage.Content = "";
+            string source = txtSource.Text;
+            string destination = txtDestination.Text;
+
+            CopyDirectory.FileCopyFacility cf = new CopyDirectory.FileCopyFacility();
+            int result = cf.DirectoryCopy(@source, @destination, true);
+
+            if (result == 0)
+            {
+                lblMessage.Content += "File Copy successfull ..";
+
+                lsbFileInfo.Items.Clear();
+                lsbFileInfo.ItemsSource = cf._listFiles;
+
+            }
+            else
+            {
+                lblMessage.Content += cf.message;
+
+            }
+
+        }
+
+    }
+}
